005cf965186380a19bb4cfb5e306fdf6c159509f
JuliaMelle <toledopascuajuliamelle@gmail.com>
2024-01-20 11:44:20 +0800

    dashboardlayout-draft1

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,141 +1,33 @@
 import Versions from './components/Versions'
 import icons from './assets/icons.svg'
-
+// import styles from "./profile.module.scss";
 function App(): JSX.Element {
   return (
     <div className="container">
-      <Versions></Versions>
-
-      <svg className="hero-logo" viewBox="0 0 900 300">
-        <use xlinkHref={`${icons}#electron`} />
-      </svg>
-      <h2 className="hero-text">
-        You{"'"}ve successfully created an Electron project with React and TypeScript
-      </h2>
-      <p className="hero-tagline">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-
-      <div className="links">
-        <div className="link-item">
-          <a target="_blank" href="https://electron-vite.org" rel="noopener noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="link-item link-dot">•</div>
-        <div className="link-item">
-          <a
-            target="_blank"
-            href="https://github.com/alex8088/electron-vite"
-            rel="noopener noreferrer"
-          >
-            Getting Help
-          </a>
-        </div>
-        <div className="link-item link-dot">•</div>
-        <div className="link-item">
-          <a
-            target="_blank"
-            href="https://github.com/alex8088/quick-start/tree/master/packages/create-electron"
-            rel="noopener noreferrer"
-          >
-            create-electron
-          </a>
-        </div>
+      <div className="column1">
+        <button className="button">Export Data</button>
+        <button className="button">Reports</button>
+      </div>
+      <div className="column1">
+        <p className="title title1"> Client Name </p>
+        <p className="title">Property Location </p>
+        <p className="title"> Document No. </p>
+        <p className="title">Most Recent Document </p>
+        <p className="title"> Date of Submission </p>
+        <p className="title t-status">Status</p>
       </div>
 
-      <div className="features">
-        <div className="feature-item">
-          <article>
-            <h2 className="title">Configuring</h2>
-            <p className="detail">
-              Config with <span>electron.vite.config.ts</span> and refer to the{' '}
-              <a target="_blank" href="https://electron-vite.org/config" rel="noopener noreferrer">
-                config guide
-              </a>
-              .
-            </p>
-          </article>
-        </div>
-        <div className="feature-item">
-          <article>
-            <h2 className="title">HMR</h2>
-            <p className="detail">
-              Edit <span>src/renderer</span> files to test HMR. See{' '}
-              <a
-                target="_blank"
-                href="https://electron-vite.org/guide/hmr.html"
-                rel="noopener noreferrer"
-              >
-                docs
-              </a>
-              .
-            </p>
-          </article>
-        </div>
-        <div className="feature-item">
-          <article>
-            <h2 className="title">Hot Reloading</h2>
-            <p className="detail">
-              Run{' '}
-              <span>
-                {"'"}electron-vite dev --watch{"'"}
-              </span>{' '}
-              to enable. See{' '}
-              <a
-                target="_blank"
-                href="https://electron-vite.org/guide/hot-reloading.html"
-                rel="noopener noreferrer"
-              >
-                docs
-              </a>
-              .
-            </p>
-          </article>
-        </div>
-        <div className="feature-item">
-          <article>
-            <h2 className="title">Debugging</h2>
-            <p className="detail">
-              Check out <span>.vscode/launch.json</span>. See{' '}
-              <a
-                target="_blank"
-                href="https://electron-vite.org/guide/debugging.html"
-                rel="noopener noreferrer"
-              >
-                docs
-              </a>
-              .
-            </p>
-          </article>
-        </div>
-        <div className="feature-item">
-          <article>
-            <h2 className="title">Source Code Protection</h2>
-            <p className="detail">
-              Supported via built-in plugin <span>bytecodePlugin</span>. See{' '}
-              <a
-                target="_blank"
-                href="https://electron-vite.org/guide/source-code-protection.html"
-                rel="noopener noreferrer"
-              >
-                docs
-              </a>
-              .
-            </p>
-          </article>
-        </div>
-        <div className="feature-item">
-          <article>
-            <h2 className="title">Packaging</h2>
-            <p className="detail">
-              Use{' '}
-              <a target="_blank" href="https://www.electron.build" rel="noopener noreferrer">
-                electron-builder
-              </a>{' '}
-              and pre-configured to pack your app.
-            </p>
-          </article>
+      <div className="card">
+        <div className="column1">
+          <div className="card-capsule"></div>
+          <div className="column2">
+            <p className="info c-name"> Jose Reyes </p>
+            <p className="info p-loc">Property Location </p>
+            <p className="info doc-no"> U052345606-R </p>
+            <div className="mrd info">Tax Clearance</div>
+            <p className="info date-sub"> 10/9/2023 </p>
+            <div className="status info">Missed</div>
+          </div>{' '}
         </div>
       </div>
     </div>
---
+++ src/renderer/src/dashboard.tsx
@@ -0,0 +1,12 @@
+import Versions from './components/Versions'
+
+function Dashboard(): JSX.Element {
+  return (
+    <div className="container">
+      <Versions></Versions>
+      HELLO DASHBOARD
+    </div>
+  )
+}
+
+export default Dashboard
