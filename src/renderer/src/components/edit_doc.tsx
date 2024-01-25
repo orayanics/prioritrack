65de261b1b34a69949f3f1113d2943063372e506
YnaSophiaDR <ynasophia.delrosario.cics@ust.edu.ph>
2024-01-26 02:00:35 +0800

    edit doc

---
+++ src/renderer/src/components/edit_doc.tsx
@@ -0,0 +1,36 @@
+import '../styles/add_docu.css'
+
+function EditDoc(): JSX.Element {
+  return (
+    <div className="card">
+      <h1 className="title">Edit a Document</h1>
+      <form>
+        <h3 className="input_title">Document No.</h3>
+        <input className="input" name="query" />
+
+        <h3 className="input_title">Document Type</h3>
+        <input className="input" name="query" />
+
+        <h3 className="input_title">Date of Submission</h3>
+        <input type="date" name="query" />
+
+        <h3 className="input_title">Status</h3>
+        <select id="status" name="status" className="select_status">
+          <option value="Missed"></option>
+          <option value="Missed">Missed</option>
+          <option value="Ongoing">Ongoing</option>
+          <option value="Upcoming">Upcoming</option>
+          <option value="Complete">Complete</option>
+          <option value="On Hold">On Hold</option>
+        </select>
+      </form>
+
+      <div className="btn2">
+        <button className="btn submit" type="submit">Submit</button>
+        <button className="btn cancel">Cancel</button>
+      </div>
+    </div>
+  )
+}
+
+export default EditDoc
