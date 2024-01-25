6ef9f424c31708c333b418bb835dd6387d611075
aliahestbn <aliah.esteban.cics@ust.edu.ph>
2024-01-25 16:53:54 +0800

    tsx change pass and forgot

---
+++ src/renderer/src/app/changepass.tsx
@@ -0,0 +1,35 @@
+import '../styles/changepass.css'
+
+function ChangePass(): JSX.Element {
+  return (
+    <body>
+      <div className="card-container">
+        <div className="card">
+          <center>
+            <h2>Change Password</h2>
+          </center>
+          <form>
+            <label htmlFor="newpass">New Password</label>
+            <input type="password" id="" name="" required />
+
+            <label htmlFor="confirmpass">Confirm Password</label>
+            <input type="password" id="" name="" required />
+
+            <div className="button-container">
+              <center>
+                <button type="submit" className="verify-button">
+                  Verify
+                </button>
+                <button type="submit" className="cancel-button">
+                  Cancel
+                </button>
+              </center>
+            </div>
+          </form>
+        </div>
+      </div>
+    </body>
+  )
+}
+
+export default ChangePass
