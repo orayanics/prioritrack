7ae474de920e042afe8d9c9586951be7d2880a2e
JuliaMelle <toledopascuajuliamelle@gmail.com>
2024-01-26 14:18:49 +0800

    gitna

--- src/renderer/src/components/add_client.tsx
+++ src/renderer/src/components/add_client.tsx
@@ -3,22 +3,24 @@ import '../styles/add_client.css'
 
 function AddClient(): JSX.Element {
   return (
-    <div className="card">
-      <h1 className="title">Add a Client</h1>
-      <form>
-        <h3 className="input_title">Client Name</h3>
-        <input className="input" name="query" />
-        <h3 className="input_title">Property Location</h3>
-        <input className="input" name="query" /> <h3 className="input_title">Client Bank Name</h3>
-        <input className="input" name="query" />
-        <h3 className="input_title">Client Bank Address</h3>
-        <input className="input" name="query" />
-      </form>
-      <div className="btn2">
-        <button className="btn submit" type="submit">
-          Submit
-        </button>
-        <button className="btn cancel ">Cancel</button>
+    <div className="container">
+      <div className="card">
+        <h1 className="title">Add a Client</h1>
+        <form>
+          <h3 className="input_title">Client Name</h3>
+          <input className="input" name="query" />
+          <h3 className="input_title">Property Location</h3>
+          <input className="input" name="query" /> <h3 className="input_title">Client Bank Name</h3>
+          <input className="input" name="query" />
+          <h3 className="input_title">Client Bank Address</h3>
+          <input className="input" name="query" />
+        </form>
+        <div className="btn2">
+          <button className="btn submit" type="submit">
+            Submit
+          </button>
+          <button className="btn cancel ">Cancel</button>
+        </div>
       </div>
     </div>
   )
