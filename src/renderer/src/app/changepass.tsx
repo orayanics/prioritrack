ce1f412cee9845380fd3718fd6f2d06479f7b6d0
JuliaMelle <toledopascuajuliamelle@gmail.com>
2024-01-26 14:45:59 +0800

    modify frontend

--- src/renderer/src/app/changepass.tsx
+++ src/renderer/src/app/changepass.tsx
@@ -3,29 +3,31 @@ import '../styles/changepass.css'
 function ChangePass(): JSX.Element {
   return (
     <body>
-      <div className="card-container">
-        <div className="card">
-          <center>
-            <h2>Change Password</h2>
-          </center>
-          <form>
-            <label htmlFor="newpass">New Password</label>
-            <input type="password" id="" name="" required />
+      <div className="container">
+        <div className="card-container">
+          <div className="card">
+            <center>
+              <h2>Change Password</h2>
+            </center>
+            <form>
+              <label htmlFor="newpass">New Password</label>
+              <input type="password" id="" name="" required />
 
-            <label htmlFor="confirmpass">Confirm Password</label>
-            <input type="password" id="" name="" required />
+              <label htmlFor="confirmpass">Confirm Password</label>
+              <input type="password" id="" name="" required />
 
-            <div className="button-container">
-              <center>
-                <button type="submit" className="verify-button">
-                  Verify
-                </button>
-                <button type="submit" className="cancel-button">
-                  Cancel
-                </button>
-              </center>
-            </div>
-          </form>
+              <div className="button-container">
+                <center>
+                  <button type="submit" className="verify-button">
+                    Verify
+                  </button>
+                  <button type="submit" className="cancel-button">
+                    Cancel
+                  </button>
+                </center>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </body>
--- src/renderer/src/app/forgotpass.tsx
+++ src/renderer/src/app/forgotpass.tsx
@@ -3,32 +3,34 @@ import '../styles/forgotpass.css'
 function ForgotPass(): JSX.Element {
   return (
     <body>
-      <div className="card-container">
-        <div className="card">
-          <center>
-            <h2>Verification Questions</h2>
-          </center>
-          <form>
-            <label htmlFor="1">1. When is your birthday?</label>
-            <input type="text" id="" name="" required />
+      <div className="container">
+        <div className="card-container">
+          <div className="card">
+            <center>
+              <h2>Verification Questions</h2>
+            </center>
+            <form>
+              <label htmlFor="1">1. When is your birthday?</label>
+              <input type="text" id="" name="" required />
 
-            <label htmlFor="q2">2. What is the name of your dog?</label>
-            <input type="text" id="" name="" required />
+              <label htmlFor="q2">2. What is the name of your dog?</label>
+              <input type="text" id="" name="" required />
 
-            <label htmlFor="q3">3. What is your mothernrs maiden name?</label>
-            <input type="text" id="" name="" required />
+              <label htmlFor="q3">3. What is your mothernrs maiden name?</label>
+              <input type="text" id="" name="" required />
 
-            <div className="button-container">
-              <center>
-                <button type="submit" className="verify-button">
-                  Verify
-                </button>
-                <button type="submit" className="cancel-button">
-                  Cancel
-                </button>
-              </center>
-            </div>
-          </form>
+              <div className="button-container">
+                <center>
+                  <button type="submit" className="verify-button">
+                    Verify
+                  </button>
+                  <button type="submit" className="cancel-button">
+                    Cancel
+                  </button>
+                </center>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </body>
